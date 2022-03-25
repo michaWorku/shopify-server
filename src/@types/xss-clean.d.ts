749c3dbd59ebde1c46cbc85f6cc58240283accb3
michaworku <mikias27worku@gmail.com>
2022-03-25 13:22:50 +0300

    api refactored

---
+++ src/@types/xss-clean.d.ts
@@ -0,0 +1,5 @@
+declare module 'xss-clean' {
+    const value: () => () => void;
+  
+    export default value;
+  }
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,11 @@ import connectDB from './connect/db'
 import log from './services/logger'
 import config from 'config'
 import morgan from 'morgan'
+import cors from 'cors'
+import cookiParser from 'cookie-parser'
+import mongoSanitize from "express-mongo-sanitize"
+import  xss from "xss-clean"
+import { rateLimit } from 'express-rate-limit';
 import errorHandler from './middlewares/errorHandler'
 import { notReachableRouteHandler } from './middlewares/notReachableRouteHandler'
 
@@ -25,8 +30,25 @@ app.use(helmet());
 const nodeEnv = config.get('NODE_ENV') as string
 if (nodeEnv) app.use(morgan('dev'));
 
+//Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!"
+});
+app.use("/api", limiter);
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(cors())
+// Cooki parser from cookies
+app.use(cookiParser());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('<h1>Hello Shopify API!</h1>');
@@ -34,12 +56,12 @@ app.get('/', (req: Request, res: Response) => {
 
 const PORT: String = config.get('PORT') as string
 
-app.use('api/auth', authRouter)
-app.use('api/users', userRouter)
-app.use('api/products', productRouter)
-app.use('api/carts', cartRouter)
-app.use('api/orders', orderRouter)
-app.use('api/checkout', stripeRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/users', userRouter)
+app.use('/api/products', productRouter)
+app.use('/api/carts', cartRouter)
+app.use('/api/orders', orderRouter)
+app.use('/api/checkout', stripeRouter)
 
 app.all('*', notReachableRouteHandler);
 app.use(errorHandler);
